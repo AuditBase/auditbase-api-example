f1b7c32a2b9891d3b13dd246b3168f00e0a449b6
risingsun007 <oab102@hotmail.com>
2024-07-23 10:25:11 -0400

    add pdf export

--- examples/js/getScan.js
+++ examples/js/getScan.js
@@ -2,7 +2,7 @@ const axios = require('axios')
 const fs = require('fs')
 
 const HOST = "https://api.auditbase.com/";
-const scan_id = "<your scan id here>";
+const scan_id = "2341f019-1e7e-491e-8f37-b77f724a381f";
 async function get_scan(scan_id) {
   api_key =  process.env.AUDITBASE_API_KEY;
   if (!api_key) {
@@ -30,5 +30,36 @@ async function get_scan(scan_id) {
   console.log("num issues:", results_report?.length);
 }
 
+async function get_scan_pdf(scan_id) {
+    api_key =  process.env.AUDITBASE_API_KEY;
+    if (!api_key) {
+      throw Error(
+        "API_KEY not found.  Please set the API_KEY environment variable."
+      );
+    }
+    webhook_url = process.env.AUDITBASE_WEBHOOK_URL || "";
+  
+    console.log("webhook_url:", webhook_url);
+    route = "v1/scans/";
+    url = HOST + route + scan_id+"?format=pdf";
+    config = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${api_key}`,
+      },
+      responseType: 'arraybuffer'
+    };
+  
+    axios.get(url, config);
+    response = await axios.get(url, config);
+    const outputPath = "scan_report222a.pdf";
+    fs.writeFileSync(outputPath, response.data);
+    //results_report = response.data;
 
-get_scan(scan_id);
+  
+    //console.log("response: ", results_report);
+    //console.log("num issues:", results_report?.length);
+  }
+
+//get_scan(scan_id);
+get_scan_pdf(scan_id);
--- examples/python/get_scan.py
+++ examples/python/get_scan.py
@@ -26,10 +26,31 @@ def get_scan(scan_id):
     print("num issues:", len(results_report))
 
 
+def get_scan_pdf(scan_id):
+    api_key = os.environ.get('AUDITBASE_API_KEY')
+    if not api_key:
+        raise(Exception("API_KEY not found.  Please set the API_KEY environment variable."))
+    
+    webhook_url = os.environ.get('AUDITBASE_WEBHOOK_URL', '')
+
+    print('webhook_url:', webhook_url)
+    route = 'v1/scans/'
+    url = HOST + route  + scan_id + '?format=pdf'
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': f'Bearer {api_key}',
+    }
+
+
+    response = requests.get(url, headers=headers)
+    with open('scan_result22bb.pdf', 'wb') as f:
+        f.write(response.content)
+
+
 
 
 if __name__ == "__main__":
-    scan_id = '<scanid you want to get>'
+    scan_id = "2341f019-1e7e-491e-8f37-b77f724a381f"
     get_scan(scan_id)
 
 
