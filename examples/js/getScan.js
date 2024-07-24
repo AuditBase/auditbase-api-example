f00256f5f7a0891b77ea641654d446e4549a8b9b
risingsun007 <oab102@hotmail.com>
2024-07-24 09:40:54 -0400

    updated schema for return of scan json

--- examples/js/getScan.js
+++ examples/js/getScan.js
@@ -1,10 +1,10 @@
-const axios = require('axios')
-const fs = require('fs')
+const axios = require("axios");
+const fs = require("fs");
 
 const HOST = "https://api.auditbase.com/";
-const scan_id = "Your scan ID here";
+const scan_id = "your scan id";
 async function get_scan(scan_id) {
-  api_key =  process.env.AUDITBASE_API_KEY;
+  api_key = process.env.AUDITBASE_API_KEY;
   if (!api_key) {
     throw Error(
       "API_KEY not found.  Please set the API_KEY environment variable."
@@ -22,44 +22,38 @@ async function get_scan(scan_id) {
     },
   };
 
-  axios.get(url, config);
   response = await axios.get(url, config);
   results_report = response.data;
-
   console.log("response: ", results_report);
-  console.log("num issues:", results_report?.length);
+  console.log("num issues:", results_report?.issues?.length);
 }
 
 async function get_scan_pdf(scan_id) {
-    api_key =  process.env.AUDITBASE_API_KEY;
-    if (!api_key) {
-      throw Error(
-        "API_KEY not found.  Please set the API_KEY environment variable."
-      );
-    }
-    webhook_url = process.env.AUDITBASE_WEBHOOK_URL || "";
-  
-    console.log("webhook_url:", webhook_url);
-    route = "v1/scans/";
-    url = HOST + route + scan_id+"?format=pdf";
-    config = {
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${api_key}`,
-      },
-      responseType: 'arraybuffer'
-    };
-  
-    axios.get(url, config);
-    response = await axios.get(url, config);
-    const outputPath = "scan_report222a.pdf";
-    fs.writeFileSync(outputPath, response.data);
-    //results_report = response.data;
-
-  
-    //console.log("response: ", results_report);
-    //console.log("num issues:", results_report?.length);
+  api_key = process.env.AUDITBASE_API_KEY;
+  if (!api_key) {
+    throw Error(
+      "API_KEY not found.  Please set the API_KEY environment variable."
+    );
   }
+  webhook_url = process.env.AUDITBASE_WEBHOOK_URL || "";
 
-//get_scan(scan_id);
-get_scan_pdf(scan_id);
+  console.log("webhook_url:", webhook_url);
+  route = "v1/scans/";
+  url = HOST + route + scan_id + "?format=pdf";
+  config = {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${api_key}`,
+    },
+    responseType: "arraybuffer",
+  };
+
+  response = await axios.get(url, config);
+  const outputPath = "scan_report.pdf";
+  fs.writeFileSync(outputPath, response.data);
+  results_report = response.data;
+
+
+}
+get_scan(scan_id);
+//get_scan_pdf(scan_id);
